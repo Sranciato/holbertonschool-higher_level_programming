16c13722aa1a2386b0f1a7b2f2a608ec5a3d8352
sranciato <sranciato1@gmail.com>
2019-09-09 18:33:30 +0000

    [LOCAL][NEW] add number 10. check if linked list is a loop

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - check if linked list is a loop.
+ * @list: head of linked list.
+ * Return: 0 if false | 1 if true.
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	turtle = list;
+	hare = list;
+
+	while (turtle && hare && hare->next)
+	{
+		turtle = turtle->next->next;
+		hare = hare->next->next->next->next;
+		if (turtle == hare)
+			return (1);
+	}
+	return (0);
+}
