889f40a47471d32df453085ab13f74874a1e3f37
sranciato <sranciato1@gmail.com>
2019-09-10 16:39:17 +0000

    [LOCAL][INIT] got last digit for negative numbers to work properly

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = abs(number) % 10
+print("Last digit of {:d} is ".format(number), end="")
+if number < 0:
+    last *= -1
+if last > 5:
+    print("{:d} and is greater than 5".format(last))
+elif last == 0:
+    print("{:d} and is 0".format(last))
+else:
+    print("{:d} and is less than 6 and not 0".format(last))
