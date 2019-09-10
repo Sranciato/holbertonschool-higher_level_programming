d0978d34f70d3f150c7d008178871d94f345fb4b
sranciato <sranciato1@gmail.com>
2019-09-10 18:24:41 +0000

    [LOCAL][INIT] made extra variable to hold new ascii value for uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def uppercase(str):
     for char in str:
+        new = char
         if ord(char) >= 97 and ord(char) <= 122:
-            new = ord(char) - 32
-            print("{}".format(chr(new)))
-        else:
-            print("{}".format(char))
+            new = chr(ord(char) - 32)
+        print("{}".format(new), end="")
+    print("")
