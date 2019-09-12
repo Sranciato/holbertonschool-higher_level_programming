266102f0c7c16640dc59b6acf82f01efb770e17f
sranciato <sranciato1@gmail.com>
2019-09-12 19:39:46 +0000

    [LOCAL][INIT] dont forget to /* when testing mul in command line

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def main():
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+        elif argv[2] == "-":
+            print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+        elif argv[2] == "*":
+            print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+        elif argv[2] == "/":
+            print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit(1)
+
+if __name__ == "__main__":
+    main()
