cc574136b7aa03d1bdc44ed68b1a60114cbf0032
sranciato <sranciato1@gmail.com>
2019-09-12 17:55:24 +0000

    [LOCAL][INIT] import sys for argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def main():
+    import sys
+    argv = sys.argv
+    if len(argv) == 1:
+        print("0 arguments.")
+        return 0
+    elif len(argv) == 2:
+        print("1 argument:")
+    elif len(argv) > 2:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for args in range(1, len(argv)):
+        print("{:d} {}".format(args, argv[args]))
+
+if __name__ == "__main__":
+    main()
