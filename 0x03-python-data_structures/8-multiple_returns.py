a75fac3d803eaebe1adaf0fc00cb74027391e2cd
sranciato <sranciato1@gmail.com>
2019-09-17 19:35:35 +0000

    [LOCAL][FIX] fix char if sentence is empty

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+
+    i = 0
     for i in range(len(sentence)):
         continue
-    char = sentence[:1]
+    if not sentence[:1]:
+        char = None
+        i = -1
+    else:
+        char = sentence[:1]
     tuple = (i + 1, char)
     return tuple
