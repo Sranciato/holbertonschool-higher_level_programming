c23764268aedb3d7aba1e3bbbe79b181c7762511
sranciato <sranciato1@gmail.com>
2019-09-19 17:35:01 +0000

    [LOCAL][FIX] use XOR operator to get every matching string excluded

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,19 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
 
-    merge = []
-    merge_2 = set()
-    set_a = set(set_1)
-    set_b = set(set_2)
-
-    merge_2 = set_a & set_b
-
-    merge.extend(set_a)
-    merge.extend(set_b)
-
-    for i in merge:
-        for x in merge_2:
-            if i == x:
-                merge.remove(i)
-
-    return merge
+    return set(set_1) ^ set(set_2)
