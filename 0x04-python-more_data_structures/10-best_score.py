60eb730b2d45243283667a4a946b87920ccc0a76
sranciato <sranciato1@gmail.com>
2019-09-19 17:43:21 +0000

    [LOCAL][FIX] use max function instead of loop to find best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
 
-    high = 0
-    best = ""
-
     if a_dictionary is None:
         return None
 
-    for key in a_dictionary.keys():
-        if a_dictionary[key] > high:
-            best = key
-            high = a_dictionary[key]
-
-    return best
+    return max(a_dictionary.keys())
