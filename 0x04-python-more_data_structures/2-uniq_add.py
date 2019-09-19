9a41c4ef574d3f8f0faee8f289627d310ef8369a
sranciato <sranciato1@gmail.com>
2019-09-19 13:15:03 +0000

    [LOCAL][ADD] add all non duplicates

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+
+    sum = 0
+    s = set()
+
+    print(s)
+    for i in range(len(my_list)):
+        if my_list[i] not in s:
+            s.add(my_list[i])
+    for i in s:
+        sum += i
+
+    return sum
