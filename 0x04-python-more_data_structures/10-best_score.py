269c744b1f9e7230967c0795b0b3e7983c02b018
sranciato <sranciato1@gmail.com>
2019-09-19 18:09:05 +0000

    [LOCAL][FIX] fix if no score return None

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,4 +4,13 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
-    return max(a_dictionary.keys())
+    high = 0
+    best = ""
+
+    for key in a_dictionary.keys():
+        if a_dictionary[key] > high:
+            best = key
+            high = a_dictionary[key]
+    if high > 0:
+        return best
+    return None
