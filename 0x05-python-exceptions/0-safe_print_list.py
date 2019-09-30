6cde29b7ad80ba4b6a093923c778491096c6156e
sranciato <sranciato1@gmail.com>
2019-09-30 17:43:51 +0000

    [BUGFIX] if x == 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,6 +3,9 @@ def safe_print_list(my_list=[], x=0):
 
     element = 0
 
+    if x == 0:
+        return(0)
+
     for element in range(0, x):
         try:
             print("{:d}".format(my_list[element]), end="")
