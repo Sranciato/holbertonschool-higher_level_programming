6e3a3d575063a9b0b91f49a39389ff84f36bc3c1
sranciato <sranciato1@gmail.com>
2019-09-30 17:49:23 +0000

    [BUGFIX] prints any value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,7 +8,7 @@ def safe_print_list(my_list=[], x=0):
 
     for element in range(0, x):
         try:
-            print("{:d}".format(my_list[element]), end="")
+            print("{}".format(my_list[element]), end="")
         except:
             element -= 1
             break
