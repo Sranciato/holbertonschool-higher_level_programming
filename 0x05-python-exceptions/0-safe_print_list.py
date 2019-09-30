a202185b6f59f9359e7211f2019daedf050ff6f8
sranciato <sranciato1@gmail.com>
2019-09-30 17:39:49 +0000

    [FEATURE] first push

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    element = 0
+
+    for element in range(0, x):
+        try:
+            print("{:d}".format(my_list[element]), end="")
+        except:
+            element -= 1
+            break
+
+    element += 1
+    print()
+    return(element)
