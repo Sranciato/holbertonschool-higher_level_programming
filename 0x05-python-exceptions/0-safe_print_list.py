584c5a30c3a28c4b48d9a32a841b69197e407f0c
sranciato <sranciato1@gmail.com>
2019-09-30 17:55:11 +0000

    [BUGFIX] print newline when x is 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
 
-    element = 0
-
-    if x == 0:
-        return(0)
+    length = 0
 
     for element in range(0, x):
         try:
             print("{}".format(my_list[element]), end="")
+            length += 1
         except:
-            element -= 1
             break
 
-    element += 1
     print()
-    return(element)
+    return(length)
