30f40a04351286fe54557f5f50feb39802125be5
sranciato <sranciato1@gmail.com>
2019-10-04 19:18:13 +0000

    [BUGIX] fixed comments

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,18 +6,24 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+    """Test cases for max_integer"""
 
     def test_correct(self):
+        """Test a case with correct values"""
         self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_same(self):
+        """Test when all of the values are the same"""
         self.assertEqual(max_integer([4, 4, 4, 4]), 4)
 
     def test_none(self):
+        """Test for empty list"""
         self.assertEqual(max_integer([]), None)
 
     def test_negative(self):
+        """Test for negative values"""
         self.assertEqual(max_integer([-3, -5, -6, -30]), -3)
 
     def test_0_args(self):
+        """Test for no arguments"""
         self.assertEqual(max_integer(), None)
