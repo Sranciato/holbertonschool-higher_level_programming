1a81970438abd0544efc4cc17f62ced2ed195e52
sranciato <sranciato1@gmail.com>
2019-10-07 18:51:45 +0000

    [FEATURE] add area and perimeter of rectangle

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" Create a rectangle """
+
+
+class Rectangle:
+    """ A rectangle """
+
+    def __init__(self, width=0, height=0):
+        """ initialize values """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Retrieve width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Set width """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise TypeError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Retrieve height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Set height """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise TypeError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """ Area of rectangle """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """ Perimeter of rectangle """
+        if self.__height is 0 or self.__width is 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
