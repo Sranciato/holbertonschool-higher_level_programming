8e78555bf56f3accdaf4a5e6e9a28ff20d299f5d
sranciato <sranciato1@gmail.com>
2019-10-15 22:27:24 +0000

    [FEATURE] add tests and validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry:
+    """empty class"""
+
+    def area(self):
+        """raises exception"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """validates [value]"""
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
