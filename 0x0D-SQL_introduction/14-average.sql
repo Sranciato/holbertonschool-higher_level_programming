96d282d0a41e330af8528313b28df92261b78191
sranciato <sranciato1@gmail.com>
2019-11-26 00:50:11 +0000

    [ADD] 15 and 16

--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,2 +1,2 @@
 -- get average of all scores in second_table
-SELECT avg(score) FROM second_table;
+SELECT avg(score) AS average FROM second_table;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- list all records of second_table
+SELECT score, name FROM second_table
+WHERE name IS NOT NULL;
