b7955871d26b5e8dd52410cd33594dca806dbeb0
sranciato <sranciato1@gmail.com>
2019-12-14 19:48:02 +0000

    [FIX] documentation comments

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Class definition for State"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 class State(Base):
+    """State class that inherits from base"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
