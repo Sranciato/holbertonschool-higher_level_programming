edf9848a06ff7bcfc472cbc5393a0170030e8bf5
root <sranciato1@gmail.com>
2020-01-06 23:10:43 +0000

    [ADD] task 6 find peak value

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" find peak value """
+
+
+def find_peak(list_of_integers):
+    """ function to find peak value in a list """
+    lis = list_of_integers
+    length = len(lis)
+
+    if lis == []:
+        return None
+    if length == 1:
+        return (lis[0])
+    peak = lis[0]
+
+    for i in range(1, len(lis)):
+        if lis[i] > peak:
+            peak = lis[i]
+
+    return (peak)
