eff0d445a9c6369cd6ec4f205d92883fcc9fb0b6
sranciato <sranciato1@gmail.com>
2020-01-13 23:31:53 +0000

    [FIX] print none if cant find

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -3,8 +3,11 @@ import requests
 from sys import argv
 
 if __name__ == "__main__":
-    my_cred = requests.get(
-        "https://api.github.com/user",
-        auth=(argv[1], argv[2])
-    ).json()
-    print(my_cred['id'])
+    try:
+        my_cred = requests.get(
+            "https://api.github.com/user",
+            auth=(argv[1], argv[2])
+        ).json()
+        print(my_cred['id'])
+    except:
+        print("None")
