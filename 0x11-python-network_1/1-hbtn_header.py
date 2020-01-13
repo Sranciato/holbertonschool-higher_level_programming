9b50e3e5e16c7458a7e1a74c647666d2337f6932
sranciato <sranciato1@gmail.com>
2020-01-13 19:54:10 +0000

    [ADD] task 1

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import urllib.request
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    with urllib.request.urlopen(url) as response:
+        print(response.getheader('X-Request-Id'))
