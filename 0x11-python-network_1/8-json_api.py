362695c57db66b144857cd43ecd64e35f58984fd
sranciato <sranciato1@gmail.com>
2020-01-13 22:06:54 +0000

    [ADD] task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+
+    if len(argv) < 2:
+        q = ""
+    else:
+        q = argv[1]
+
+    try:
+        response = requests.post(
+            "http://0.0.0.0:5000/search_user",
+            data={'q': q}
+        ).json()
+    except:
+        print("Not a valid JSON")
+    if response == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(response['id'], response['name']))
