46077435d7f6a051dc91aaee18cf41c53d2d8c85
sranciato <sranciato1@gmail.com>
2020-02-04 19:48:39 +0000

    [FIX] if last number has many of the same

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,5 +2,12 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  console.log(process.argv.sort()[process.argv.length - 2]);
+  let max = 0;
+  const sorted = process.argv.sort().slice(2);
+  for (let i = 0; i < sorted.length; i++) {
+    if (sorted[i] > max && sorted[i] < sorted[sorted.length - 1]) {
+      max = sorted[i];
+    }
+  }
+  console.log(max);
 }
