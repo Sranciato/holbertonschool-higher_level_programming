b91a58ec8f481bd47fc87e457f37c1db170e13a4
sranciato <sranciato1@gmail.com>
2020-02-04 19:06:34 +0000

    [FIX] semistandard for 4 and 13

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,2 +1,6 @@
 #!/usr/bin/node
-console.log(process.argv[2].concat(' is ', process.argv[3]));
+if (process.argv[2] === undefined) {
+  console.log('undefined is undefined');
+} else {
+  console.log(process.argv[2].concat(' is ', process.argv[3]));
+}
