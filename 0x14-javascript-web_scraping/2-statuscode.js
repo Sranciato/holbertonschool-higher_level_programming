ea8ce85f093673dd296e1ad0988a611b928dc509
sranciato <sranciato1@gmail.com>
2020-02-11 20:56:07 +0000

    [ADD] task 3

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,5 @@
 const request = require('request');
 const url = process.argv[2];
 request.get(url).on('response', function (response) {
-  console.log('Code: ' + response.statusCode);
+  console.log('ode: ' + response.statusCode);
 });
