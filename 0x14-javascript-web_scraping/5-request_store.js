864b937ba41f3b5d628cfab2c10cf8cb8c7c9599
sranciato <sranciato1@gmail.com>
2020-02-11 22:59:04 +0000

    [ADD] task 5 fix semistandard

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const fs = require('fs');
-let url = process.argv[2];
-let file = process.argv[3];
+const url = process.argv[2];
+const file = process.argv[3];
 request.get(url).pipe(fs.createWriteStream(file, 'utf-8'));
