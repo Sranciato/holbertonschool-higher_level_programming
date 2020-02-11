d2ba70daac35b56098a9f3fb34955c21d8ed99be
sranciato <sranciato1@gmail.com>
2020-02-11 21:56:32 +0000

    [ADD] task 5

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-const peopleUrl = 'https://swapi.co/api/people/18/';
 request.get(url, function (error, response, body) {
   if (error) {
     return;
@@ -9,7 +8,7 @@ request.get(url, function (error, response, body) {
   let count = 0;
   for (const movie of JSON.parse(body).results) {
     for (const people of movie.characters) {
-      if (people === peopleUrl) { count++; }
+      if (people.indexOf('/18/') >= 0) { count += 1; }
     }
   }
   console.log(count);
