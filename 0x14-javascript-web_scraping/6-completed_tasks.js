3583b97a2f8af5b32a12266ec116ceb82b9f382e
sranciato <sranciato1@gmail.com>
2020-02-11 23:38:14 +0000

    [ADD] task 7

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const url = process.argv[2];
+request.get(url, function (error, response, body) {
+  if (error) { throw error; }
+  const dict = {};
+  let count = 0;
+  let prevId = 1;
+  for (const user of JSON.parse(body)) {
+    if (user.userId > prevId) {
+      prevId = user.userId;
+      count = 0;
+    }
+    if (user.completed === true) {
+      dict[prevId] = count += 1;
+    }
+  }
+  console.log(dict);
+});
